6e30c6f3d6b0ae2b1cfafa907ba9b494ebc528e7
LeaderbotX400 <eric@singerfamily.ca>
2023-11-15 14:16:19 -0700

    save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TestVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TestVision.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode.opModes;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.SelectCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.commands.BlobDetect;
-import org.firstinspires.ftc.teamcode.commands.RunCheck;
+import org.firstinspires.ftc.teamcode.commands.DriveToPose;
 import org.firstinspires.ftc.teamcode.opModes.base.AutoOpMode;
 import org.firstinspires.ftc.teamcode.processors.BlobProcessor;
 import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
@@ -22,11 +24,10 @@ public class TestVision extends AutoOpMode {
 			new SequentialCommandGroup(
 				new BlobDetect(v),
 				new SelectCommand(
-					// the first parameter is a map of commands
 					new HashMap<Object, Command>() {{
-						put(BlobProcessor.Selected.LEFT, new RunCheck("Left", telemetrySubsystem));
-						put(BlobProcessor.Selected.MIDDLE, new RunCheck("Middle", telemetrySubsystem));
-						put(BlobProcessor.Selected.RIGHT, new RunCheck("Right", telemetrySubsystem));
+						put(BlobProcessor.Selected.LEFT, new DriveToPose(d, o, t, new Pose2d(-10,0,Rotation2d.fromDegrees(0))));
+						put(BlobProcessor.Selected.MIDDLE, new DriveToPose(d, o, t, new Pose2d(0,10,Rotation2d.fromDegrees(0))));
+						put(BlobProcessor.Selected.RIGHT, new DriveToPose(d, o, t, new Pose2d(10,0,Rotation2d.fromDegrees(0))));
 					}},
 					// the selector
 					v::getBlob
